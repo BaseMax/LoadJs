4e7a4dfd70d414596b9f9422616b4612553edfaa
Max Base <MaxBaseCode@Gmail.Com>
2019-01-16 15:06:16 +0330

    Update Load.js

--- Load.js
+++ Load.js
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : Load.js
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-01-16
+* @Released under : https://github.com/BaseMax/LoadJs/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/LoadJs
+*
+**/
